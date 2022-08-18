b45d2d61d36158ea353e2aae20bf666b7c83df6e
mariana280721 <mariana.nunez@gfe.frre.utn.edu.ar>
2022-08-18 20:23:08 -0300

    Add report fix

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -2,7 +2,7 @@ const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
     jsonDir: "cypress/cucumber-json",  // ** Path of .json file **//
-    reportPath: "./reports/cucumber-htmlreport.html",
+    reportPath: "./reports/cucumber-html",
     metadata: {
         browser: {
             name: "chrome",
@@ -13,5 +13,15 @@ report.generate({
             name: "Windows",
             version: "10",
         },
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            {label: 'Project', value: 'Custom project'},
+            {label: 'Release', value: '1.2.3'},
+            {label: 'Cycle', value: 'B11221.34321'},
+            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+        ]
     },
 });
